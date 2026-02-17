69ccb6846ca5b8f95dbbfce95b0a9e651040a6bd
xaviivars <xaviivarsausina@gmail.com>
2026-02-17 12:03:48 +0100

    style: Minor changes in Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,12 +3,28 @@ type ButtonProps = {
 }
 
 export function Button ({ children }: ButtonProps) {
-
     return (
-        <button className="w-80 h-12 mx-auto bg-amber-400 rounded-4xl flex items-center justify-center
-                       font-semibold text-black text-lg">
-            {children}
-        </button>
-    )
-
+        <button
+            className="
+                w-80
+                h-12
+                mx-auto
+                bg-amber-400
+                text-black
+                text-lg
+                font-semibold
+                rounded-full
+                flex items-center justify-center
+                shadow-lg
+                transition-all duration-150 ease-in-out
+                hover:bg-amber-500
+                hover:shadow-xl
+                active:scale-95
+                active:shadow-md
+                cursor-pointer
+            "
+            >
+      {children}
+    </button>
+  )
 }
