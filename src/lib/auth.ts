e3bf15bc1573e954f4d59c27fd542ab2743ddeb2
xaviivars <xaviivarsausina@gmail.com>
2026-02-18 19:48:28 +0100

    feat: Configure Prisma ORM schema and integrate Better-Auth with PostgreSQL

---
+++ src/lib/auth.ts
@@ -0,0 +1,20 @@
+import { betterAuth } from "better-auth"
+import { prismaAdapter } from "better-auth/adapters/prisma"
+import { PrismaClient } from "../generated/prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL!, });
+const prisma = new PrismaClient({ adapter })
+
+export const auth = betterAuth ({
+    database: prismaAdapter(prisma, { provider: "postgresql" }),
+    emailAndPassword: {
+        enabled: true,
+    },
+    socialProviders: {
+        google: {
+            clientId: "",
+            clientSecret: "",
+        }
+    }
+})
